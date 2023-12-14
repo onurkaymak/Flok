49b29f30e2ed5f149858312d166c6e10f54516a0
onurkaymak <onurkaymak@outlook.com>
2023-12-14 06:43:50 -0800

    Add Browser Router and Store Provider in index.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './components/AppApp';
+import App from './components/App';
+
+import { BrowserRouter, } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './components/store/index';
 
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
