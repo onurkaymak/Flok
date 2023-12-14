71c826a57a5ac41f1ec2a7305f68bffc39dc96d4
onurkaymak <onurkaymak@outlook.com>
2023-12-14 06:38:35 -0800

    Add routing with React Router.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
+import { Routes, Route, Navigate } from 'react-router';
 import { useSelector } from 'react-redux';
+
+import Home from './pages/Home';
+import Auth from './pages/Auth';
+import Profile from './pages/Profile';
 
 const App = () => {
   const isLoggedIn = useSelector(state => state.user.isLoggedIn);
 
-
   return (
     <div className="App">
-
+      <Routes>
+        {!isLoggedIn && (<Route path='/' element={<Home />} />)}
+        {isLoggedIn && (<Route path='/' element={<Navigate to={'/profile'} />} />)}
+        {isLoggedIn && (<Route path='/profile' element={<Profile />} />)}
+        {!isLoggedIn && (<Route path='/profile' element={<Auth />} />)}
+      </Routes>
     </div>
   );
 }
