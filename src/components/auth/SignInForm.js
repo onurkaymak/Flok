3659f91278d798063f3eec451e3ff033babe612e
onurkaymak <onurkaymak@outlook.com>
2023-12-15 10:46:24 -0800

    Add inputRef with useRef hook

--- src/components/auth/SignInForm.js
+++ src/components/auth/SignInForm.js
@@ -1,6 +1,9 @@
+import { useRef } from 'react';
 import './SignInForm.css';
 
 const SignInForm = (props) => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
 
   return (
@@ -45,6 +48,7 @@ const SignInForm = (props) => {
                   type="email"
                   autoComplete="email"
                   required
+                  ref={emailInputRef}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -65,6 +69,7 @@ const SignInForm = (props) => {
                   type="password"
                   autoComplete="current-password"
                   required
+                  ref={passwordInputRef}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
