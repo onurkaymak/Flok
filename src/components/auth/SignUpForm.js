19011e4ec436d98aa3627791ddae60e07293b787
onurkaymak <onurkaymak@outlook.com>
2023-12-14 22:34:52 -0800

    Add styling.

---
+++ src/components/auth/SignUpForm.js
@@ -0,0 +1,200 @@
+import { useState, Fragment } from 'react'
+import { ChevronDownIcon, CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { Switch, Listbox, Transition } from '@headlessui/react'
+import './SignInForm.css';
+
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const people = [
+  {
+    id: 1,
+    name: 'Auto Detailer',
+    avatar:
+      'https://i.ibb.co/J2t8rb4/cleaning.png',
+  },
+  {
+    id: 2,
+    name: 'Customer Service Agent',
+    avatar:
+      'https://i.ibb.co/34bsw2G/car-rent.png',
+  },
+  {
+    id: 3,
+    name: 'Manager',
+    avatar:
+      'https://i.ibb.co/n7zmdJB/project-management.png',
+  },
+]
+
+
+const SignUpForm = () => {
+  const [selected, setSelected] = useState(people[0]);
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log("here")
+  };
+
+
+
+  return (
+    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Create an account</h2>
+      </div>
+      <form className="mx-auto mt-16 max-w-sm sm:mt-20 group" onSubmit={formSubmitHandler} noValidate autoComplete='off'>
+        <div className="flex flex-col">
+
+          <div className="mb-5">
+            <label htmlFor="name" className="block text-sm font-semibold leading-6 text-gray-900">
+              Employee Name
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                autoComplete="off"
+                required
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
+              />
+              <span className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                Please enter a valid email address
+              </span>
+
+            </div>
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+              Email
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                autoComplete="off"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
+              />
+            </div>
+          </div>
+
+          <div className="mb-5">
+            <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
+              Password
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                autoComplete="off"
+                required
+                pattern=".{6,}"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
+              />
+            </div>
+          </div>
+
+          <Listbox value={selected} onChange={setSelected}>
+            {({ open }) => (
+              <>
+                <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Employee Role</Listbox.Label>
+                <div className="relative mt-2">
+                  <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                    <span className="flex items-center">
+                      <img src={selected.avatar} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
+                      <span className="ml-3 block truncate">{selected.name}</span>
+                    </span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                      <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                    </span>
+                  </Listbox.Button>
+
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                      {people.map((person) => (
+                        <Listbox.Option
+                          key={person.id}
+                          className={({ active }) =>
+                            classNames(
+                              active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                              'relative cursor-default select-none py-2 pl-3 pr-9'
+                            )
+                          }
+                          value={person}
+                        >
+                          {({ selected, active }) => (
+                            <>
+                              <div className="flex items-center">
+                                <img src={person.avatar} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
+                                <span
+                                  className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
+                                >
+                                  {person.name}
+                                </span>
+                              </div>
+
+                              {selected ? (
+                                <span
+                                  className={classNames(
+                                    active ? 'text-white' : 'text-indigo-600',
+                                    'absolute inset-y-0 right-0 flex items-center pr-4'
+                                  )}
+                                >
+                                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </>
+            )}
+          </Listbox>
+
+        </div>
+
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 group-invalid:pointer-events-none group-invalid:opacity-30"
+          >
+            Create an account
+          </button>
+        </div>
+      </form >
+    </div >
+  )
+}
+
+
+export default SignUpForm;
