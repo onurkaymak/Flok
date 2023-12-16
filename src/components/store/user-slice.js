bd04de6a0cf7d81eef1f9d19826056c29c09917e
onurkaymak <onurkaymak@outlook.com>
2023-12-15 16:53:17 -0800

    Refactor user slice.

--- src/components/store/user-slice.js
+++ src/components/store/user-slice.js
@@ -13,12 +13,13 @@ const userSlice = createSlice({
     login(state, action) {
       state.userId = action.payload.userId;
       state.token = action.payload.token;
-      state.tokenExpTime = action.payload.token;
+      state.tokenExpTime = action.payload.tokenExpTime;
       state.userRole = action.payload.userRole;
     },
     logout(state) {
       state.userId = null;
       state.token = null;
+      state.tokenExpTime = null;
     },
     setIsLoggedIn(state, action) {
       state.isLoggedIn = action.payload;
