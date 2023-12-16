bf8815e0e134d7b9844ee8dfe15663179ee022fb
onurkaymak <onurkaymak@outlook.com>
2023-12-16 12:43:49 -0800

    Add conditional rendering in Profile.js

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import ProfileNavbar from "../layout/ProfileNavbar";
 import ManagerDash from "../roles/ManagerDash";
-
 import classes from "./Profile.module.css";
 
 
 const Profile = () => {
+  const [currentManagerDash, setCurrentManagerDash] = useState("Fleet");
 
-
+  const dashChoiceHandler = (chosenDash) => {
+    setCurrentManagerDash(chosenDash);
+  };
 
   return (
     <div className={classes.test}>
-      <ProfileNavbar />
-      <ManagerDash />
+      <ProfileNavbar onManagerDashChoice={dashChoiceHandler} />
+      <ManagerDash currentDash={currentManagerDash} />
     </div>
 
   )
