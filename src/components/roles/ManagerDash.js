5fc11566a4ab6cf6c79d67126f4ab79f8ff30bf8
onurkaymak <onurkaymak@outlook.com>
2023-12-16 20:09:01 -0800

    Create ManageDash component.

--- src/components/roles/ManagerDash.js
+++ src/components/roles/ManagerDash.js
@@ -1,19 +1,63 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import classes from './ManagerDash.module.css';
 
 
-const ManagerDash = () => {
-  const [currentDash, setCurrentDash] = useState("Fleet");
+const ManagerDash = (props) => {
+  const [sideBarLink, setSideBarLink] = useState(null);
 
-  const fleetDash =
-    <div className={classes.sidebarContainer}>
-      <button className={classes.sidebarAddButton}>Add New Vehicle</button>
-      <ul className={classes.listContainer}>
-        <li className={classes.listItem}>Search For a Vehicle</li>
-        <li className={classes.listItem}>Update a Vehicle</li>
-        <li className={classes.listItem}>Delete a Vehicle</li>
-      </ul>
-    </div>
+  useEffect(() => {
+    const fleetLinks =
+      <div className={classes.sidebarContainer}>
+        <button className={classes.sidebarTopButton}>Add New Vehicle</button>
+        <ul className={classes.listContainer}>
+
+          <div className={classes.listItemContainer}>
+            <li className={classes.listItem}>Search For a Vehicle</li>
+          </div>
+          <div className={classes.listItemContainer}>
+            <li className={classes.listItem}>Update a Vehicle</li>
+          </div>
+          <div className={classes.listItemContainer}>
+            <li className={classes.listItem}>Delete a Vehicle</li>
+          </div>
+        </ul>
+      </div>
+
+    const productionLinks =
+      <div className={classes.sidebarContainer}>
+        <button className={classes.sidebarTopButton}>Start New Detailing</button>
+        <ul className={classes.listContainer}>
+          <li className={classes.listItem}>Pending Detailings</li>
+          <li className={classes.listItem}>Detailer Performance</li>
+        </ul>
+      </div>
+
+    const rentalLinks =
+      <div className={classes.sidebarContainer}>
+        <button className={classes.sidebarTopButton}>Book a Vehicle</button>
+        <ul className={classes.listContainer}>
+          <li className={classes.listItem}>Check Pending Rentals</li>
+          <li className={classes.listItem}>Update Pending Rental Information</li>
+          <li className={classes.listItem}>Delete Pending Rental</li>
+        </ul>
+      </div>
+
+
+    switch (props.currentDash) {
+      case 'Fleet':
+        setSideBarLink(fleetLinks);
+        break;
+      case 'Production':
+        setSideBarLink(productionLinks);
+        break;
+      case 'Rental':
+        setSideBarLink(rentalLinks);
+        break;
+      default:
+        setSideBarLink(fleetLinks);
+    }
+  }, [props.currentDash]);
+
 
 
 
@@ -22,7 +66,7 @@ const ManagerDash = () => {
 
   return (
     <div className={classes.container}>
-      {fleetDash}
+      {sideBarLink}
 
       <h1>Test</h1>
 
