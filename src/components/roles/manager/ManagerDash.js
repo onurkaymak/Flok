31ff321764208e47f6f268e069eae1cb936efd35
onurkaymak <onurkaymak@outlook.com>
2023-12-17 11:48:42 -0800

    Create state management for selected links.

--- src/components/roles/manager/ManagerDash.js
+++ src/components/roles/manager/ManagerDash.js
@@ -2,35 +2,74 @@ import { useState, useEffect } from 'react';
 import classes from './ManagerDash.module.css';
 
 import sprite from '../../img/sprite.svg';
-import SearchResult from './SearchResult';
+
+import FleetManager from './fleet/FleetManager';
 
 
 const ManagerDash = (props) => {
   const [sideBarLink, setSideBarLink] = useState(null);
 
+  const [selectedFleetLink, setSelectedFleetLink] = useState(null);
+  const [selectedProductionLink, setSelectedProductionLink] = useState(null);
+  const [selectedRentalLink, setSelectedRentalLink] = useState(null);
+
+  const selectedLinkHandler = (selectedLink) => {
+    switch (selectedLink) {
+      case 'search fleet':
+        setSelectedFleetLink("search")
+        break;
+      case 'add fleet':
+        setSelectedFleetLink("add")
+        break;
+      case 'update fleet':
+        setSelectedFleetLink("update")
+        break;
+      case 'delete fleet':
+        setSelectedFleetLink("delete")
+        break;
+      case 'search production':
+        setSelectedFleetLink("search")
+        break;
+      case 'add production':
+        setSelectedFleetLink("search")
+        break;
+      case 'search2 production':
+        setSelectedFleetLink("search")
+        break;
+
+
+
+
+
+
+      default:
+        setSelectedFleetLink(null);
+    }
+  };
+
   useEffect(() => {
     const fleetLinks =
       <div className={classes.sidebarContainer}>
-        <div className={classes.sidebarTopButtonContainer}>
+        <div className={classes.sidebarTopButtonContainer} onClick={() => selectedLinkHandler("search fleet")}>
           <button className={classes.sidebarTopButton}>Vehicle</button>
           <svg className={classes.iconSearch}>
             <use href={sprite + "#icon-search"}></use>
           </svg>
         </div>
         <ul className={classes.listContainer}>
-          <div className={classes.listItemContainer}>
+          <div className={classes.listItemContainer} onClick={() => selectedLinkHandler("add fleet")}>
             <li className={classes.listItem}>Add Vehicle</li>
             <svg className={classes.listItemIcon}>
               <use href={sprite + "#icon-plus"}></use>
             </svg>
           </div>
-          <div className={classes.listItemContainer}>
+          <div className={classes.listItemContainer} onClick={() => selectedLinkHandler("update fleet")}>
             <li className={classes.listItem}>Update Vehicle</li>
             <svg className={classes.listItemIcon}>
               <use href={sprite + "#icon-loop2"}></use>
             </svg>
           </div>
-          <div className={classes.listItemContainer}>
+          <div className={classes.listItemContainer} onClick={() => selectedLinkHandler("delete fleet")}>
             <li className={classes.listItem}>Delete Vehicle</li>
             <svg className={classes.listItemIcon}>
               <use href={sprite + "#icon-bin2"}></use>
@@ -42,20 +81,20 @@ const ManagerDash = (props) => {
 
     const productionLinks =
       <div className={classes.sidebarContainer}>
-        <div className={classes.sidebarTopButtonContainer}>
+        <div className={classes.sidebarTopButtonContainer} onClick={() => selectedLinkHandler("search production")}>
           <button className={classes.sidebarTopButton}>Detailings</button>
           <svg className={classes.iconSearch}>
             <use href={sprite + "#icon-search"}></use>
           </svg>
         </div>
         <ul className={classes.listContainer}>
-          <div className={classes.listItemContainer}>
+          <div className={classes.listItemContainer} onClick={() => selectedLinkHandler("add production")}>
             <li className={classes.listItem}>New Detailing</li>
             <svg className={classes.listItemIcon}>
               <use href={sprite + "#icon-plus"}></use>
             </svg>
           </div>
-          <div className={classes.listItemContainer}>
+          <div className={classes.listItemContainer} onClick={() => selectedLinkHandler("search2 production")}>
             <li className={classes.listItem}>Productivity</li>
             <svg className={classes.listItemIcon}>
               <use href={sprite + "#icon-users"}></use>
@@ -115,7 +154,7 @@ const ManagerDash = (props) => {
   return (
     <div className={classes.container}>
       {sideBarLink}
-      <SearchResult />
+      {props.currentDash === "Fleet" && <FleetManager onLinkSelection={selectedFleetLink} />}
     </div>
   )
 };
