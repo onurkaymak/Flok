b3d46fc1100781d768d3edd5aa0ae628b2de25bc
onurkaymak <onurkaymak@outlook.com>
2023-12-17 18:43:21 -0800

    Add fetch reducer.

--- src/components/store/fleet-slice.js
+++ src/components/store/fleet-slice.js
@@ -7,7 +7,24 @@ const fleetSlice = createSlice({
   },
   reducers: {
     fetch(state, action) {
+      const data = action.payload;
 
+      data.map(vehicle => (
+        state.vehicles.push({
+          key: vehicle.vehicleId,
+          vehicleId: vehicle.vehicleId,
+          vin: vehicle.vin,
+          make: vehicle.make,
+          model: vehicle.model,
+          mileage: vehicle.mileage,
+          class: vehicle.class,
+          classCode: vehicle.classCode,
+          state: vehicle.state,
+          licensePlate: vehicle.licensePlate,
+          isRented: vehicle.isRented,
+          inProduction: vehicle.inProduction
+        })
+      ))
     }
   }
 });
