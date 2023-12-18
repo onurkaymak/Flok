a829c7e04d25e64570b3f9bea3d26158518e8c81
onurkaymak <onurkaymak@outlook.com>
2023-12-17 16:54:11 -0800

    Create fleet-actions, create fetchVehicles action.

---
+++ src/components/store/fleet-actions.js
@@ -0,0 +1,23 @@
+import { fleetActions } from "./fleet-slice";
+
+import axios from "axios";
+
+
+export const fetchVehicles = (queries) => {
+  return async (dispatch) => {
+
+    const { VIN, isRented, inProduction, token } = queries;
+
+    try {
+      const response = await axios.get("http://localhost:5000/fleet", {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+
+      console.log(response);
+    }
+    catch (err) {
+
+    }
+
+  }
+}
