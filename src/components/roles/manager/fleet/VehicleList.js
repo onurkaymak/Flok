fd60e0c69253819f2893a5ca8bc7dee545363fe0
onurkaymak <onurkaymak@outlook.com>
2023-12-18 22:43:43 -0800

    Add list of data with MUI.

--- src/components/roles/manager/fleet/VehicleList.js
+++ src/components/roles/manager/fleet/VehicleList.js
@@ -1,12 +1,42 @@
-import Vehicle from "./Vehicle";
+import React from "react";
+import { DataGrid } from '@mui/x-data-grid';
 
+const columns = [
+  { field: 'id', headerName: 'Id', width: 50 },
+  { field: 'vin', headerName: 'VIN', width: 90 },
+  { field: 'make', headerName: 'Make', width: 90 },
+  { field: 'model', headerName: 'Model', width: 90 },
+  { field: 'mileage', headerName: 'Mileage', type: 'number', width: 70 },
+  { field: 'class', headerName: 'Class', width: 110 },
+  { field: 'classCode', headerName: 'Class Code', width: 90 },
+  { field: 'state', headerName: 'State', width: 50 },
+  { field: 'licensePlate', headerName: 'License Plate', width: 100 },
+  { field: 'isRented', headerName: 'Rent Status', width: 90 },
+  { field: 'inProduction', headerName: 'Production Status', width: 125 },
+
+
+
+];
 
 const VehicleList = (props) => {
 
-
   return (
-    <div>
-      {props.fetchedVehicles.map(vehicle =>
+    <React.Fragment>
+      <div style={{ height: 1000, width: '100%' }}>
+        <DataGrid
+          rows={props.fetchedVehicles}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      </div>
+
+      {/* {props.fetchedVehicles.map(vehicle =>
         <Vehicle
           key={vehicle.vehicleId}
           vehicleId={vehicle.vehicleId}
@@ -17,8 +47,8 @@ const VehicleList = (props) => {
           isRented={vehicle.isRented}
           inProduction={vehicle.inProduction}
         />
-      )}
-    </div>
+      )} */}
+    </React.Fragment>
   )
 };
 
