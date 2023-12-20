6f82582be843370c4006251637919727fd5022eb
onurkaymak <onurkaymak@outlook.com>
2023-12-20 06:10:03 -0800

    WIP AddRentalForm event.

--- src/components/roles/manager/rental/AddRentalForm.js
+++ src/components/roles/manager/rental/AddRentalForm.js
@@ -5,6 +5,8 @@ const AddRentalForm = (props) => {
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    console.log(event);
   }
 
   const cancelButtonClickHandler = () => {
