fd336a027ebc926a60ae1507b4ada2f29e3d2595
onurkaymak <onurkaymak@outlook.com>
2023-12-20 13:54:50 -0800

    Create fetchRentalService action in rental actions.

--- src/components/store/rental-actions.js
+++ src/components/store/rental-actions.js
@@ -3,6 +3,37 @@ import axios from "axios";
 
 
 
+export const fetchRentalService = (rentalServiceInfo) => {
+  return async (dispatch) => {
+    const { rentalServiceId, customerEmail, customerPhoneNum, token } = rentalServiceInfo;
+
+    let url = "https://localhost:5000/api/rental";
+
+    if (rentalServiceId) {
+      url = `${url}?rentalServiceId=${rentalServiceId}`;
+    }
+    if (customerEmail) {
+      url = `${url}?customerEmail=${customerEmail}`;
+    }
+    if (customerPhoneNum) {
+      url = `${url}?phoneNum=${customerPhoneNum}`;
+    }
+
+    try {
+      const response = await axios.get(url);
+
+      console.log(response);
+
+      // const fetchedVehicles = response.data;
+
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+
 export const addRentalService = (reservationInfo, token) => {
   return async (dispatch) => {
 
