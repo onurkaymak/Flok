003207cd97fc81bfb11de5352e7df8ac7dfa8e25
onurkaymak <onurkaymak@outlook.com>
2023-12-21 07:31:57 -0800

    Update fetchRentalServiceList in rental-slice.

--- src/components/store/rental-slice.js
+++ src/components/store/rental-slice.js
@@ -9,8 +9,25 @@ const rentalSlice = createSlice({
   reducers: {
     fetchSelectedRentalService(state, action) {
       const data = action.payload;
-
       state.selectedRentalService = { ...data };
+    },
+    fetchRentalServiceList(state, action) {
+      const data = action.payload;
+      data.map(service => (
+        state.rentalServices.push({
+          key: service.rentalServiceId,
+          id: service.rentalServiceId,
+          contactName: service.customer.name,
+          contactEmail: service.customer.email,
+          contactNum: service.customer.phoneNum,
+          pickUpTime: service.reservationStart,
+          returnTime: service.reservationEnd,
+          make: service.vehicle.make,
+          model: service.vehicle.model,
+          vin: service.vehicle.vin,
+          color: service.vehicle.color
+        })
+      ))
     },
     add(state, action) {
       state.rentals.push(action.payload);
