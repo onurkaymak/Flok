7a492562c04e54273c3fb070574823a596cf4a36
onurkaymak <onurkaymak@outlook.com>
2023-12-21 07:18:34 -0800

    Create fetchRentalServiceList action.

--- src/components/store/rental-actions.js
+++ src/components/store/rental-actions.js
@@ -1,6 +1,7 @@
 import { rentalActions } from "./rental-slice";
 import axios from "axios";
 
+
 export const fetchRentalService = (reservationInfo, token) => {
   return async (dispatch) => {
     const { rentalServiceId, customerEmail, customerPhoneNum } = reservationInfo;
@@ -23,6 +24,22 @@ export const fetchRentalService = (reservationInfo, token) => {
       const fetchedRentalService = response.data[0];
       dispatch(rentalActions.fetchSelectedRentalService(fetchedRentalService));
 
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+
+export const fetchRentalServiceList = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get("https://localhost:5000/api/rental");
+
+      const fetchedRentalServiceList = response.data;
+
+      dispatch(rentalActions.fetchRentalServiceList(fetchedRentalServiceList));
     }
     catch (err) {
       console.log(err);
