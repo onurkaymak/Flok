831c2360c1ac2173cea1af161e89f913582ef0c2
onurkaymak <onurkaymak@outlook.com>
2023-12-23 18:06:55 -0800

    Change pageSizeOptions.

--- src/components/roles/manager/fleet/VehicleList.js
+++ src/components/roles/manager/fleet/VehicleList.js
@@ -26,10 +26,10 @@ const VehicleList = (props) => {
           columns={columns}
           initialState={{
             pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
+              paginationModel: { page: 0, pageSize: 10 },
             },
           }}
-          pageSizeOptions={[5, 10]}
+          pageSizeOptions={[5, 10, 20]}
           checkboxSelection
         />
       </div>
