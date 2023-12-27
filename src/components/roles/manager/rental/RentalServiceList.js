54f7c330a2706ef35c736bd0c598c4b436adaddc
onurkaymak <onurkaymak@outlook.com>
2023-12-26 21:21:46 -0800

    Add state for warning modal.

--- src/components/roles/manager/rental/RentalServiceList.js
+++ src/components/roles/manager/rental/RentalServiceList.js
@@ -10,6 +10,7 @@ import RentalServiceListTable from './RentalServiceListTable';
 const RentalServiceList = (props) => {
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
+  const [warningModal, setWarningModal] = useState(false);
 
 
   const cancelButtonRef = useRef(null)
@@ -49,6 +50,7 @@ const RentalServiceList = (props) => {
   const cancelButtonClickHandler = () => {
 
   };
+
 
 
   return (
@@ -181,7 +183,8 @@ const RentalServiceList = (props) => {
                   leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
                   <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+
+                    {!warningModal && <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                       <div className="sm:flex sm:items-start">
                         <div className="mx-auto flex h-14 w-14 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
                           <InformationCircleIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
@@ -257,11 +260,10 @@ const RentalServiceList = (props) => {
                                 </dl>
                               </div>
                             </div>
-
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </div>}
                     <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                       <button
                         type="button"
@@ -270,6 +272,7 @@ const RentalServiceList = (props) => {
                       >
                         Okay
                       </button>
+
                     </div>
                   </Dialog.Panel>
                 </Transition.Child>
@@ -278,6 +281,10 @@ const RentalServiceList = (props) => {
           </Dialog>
         </Transition.Root>
       </div>
+
+
+
+
       <div className={classes.allRentalServicesContainer}>
         <RentalServiceListTable fetchedRentalServiceList={fetchedRentalServiceList} />
       </div>
