11c030cd582e17164db87ac5561a65a389ec0202
onurkaymak <onurkaymak@outlook.com>
2023-12-26 21:24:46 -0800

    Add delete button, add click event listener.

--- src/components/roles/manager/rental/RentalServiceList.js
+++ src/components/roles/manager/rental/RentalServiceList.js
@@ -275,7 +275,13 @@ const RentalServiceList = (props) => {
                       >
                         Okay
                       </button>
-
+                      <button
+                        type="button"
+                        className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                        onClick={() => deleteButtonHandler()}
+                      >
+                        Delete
+                      </button>
                     </div>
                   </Dialog.Panel>
                 </Transition.Child>
