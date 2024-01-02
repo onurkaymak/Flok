58b8f4d4bc0c976b4b93785be856debae9eb8b8a
onurkaymak <onurkaymak@outlook.com>
2024-01-01 21:12:53 -0800

    Change button's order in modal.

--- src/components/roles/manager/rental/RentalServiceList.js
+++ src/components/roles/manager/rental/RentalServiceList.js
@@ -303,17 +303,17 @@ const RentalServiceList = (props) => {
                     <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                       <button
                         type="button"
-                        className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
-                        onClick={modalCloseHandler}
-                      >
-                        Okay
-                      </button>
-                      <button
-                        type="button"
                         className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                         onClick={() => deleteButtonHandler()}
                       >
                         Delete
+                      </button>
+                      <button
+                        type="button"
+                        className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
+                        onClick={modalCloseHandler}
+                      >
+                        Close
                       </button>
                     </div>
                   </Dialog.Panel>
