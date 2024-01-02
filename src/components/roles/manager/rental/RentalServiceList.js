734b804b4d8e49b49b586c7ed8d4677a229d8e2e
onurkaymak <onurkaymak@outlook.com>
2024-01-01 21:25:32 -0800

    Add ternary operator to render close/cancel button.

--- src/components/roles/manager/rental/RentalServiceList.js
+++ src/components/roles/manager/rental/RentalServiceList.js
@@ -313,7 +313,7 @@ const RentalServiceList = (props) => {
                         className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
                         onClick={modalCloseHandler}
                       >
-                        Close
+                        {warningModal ? "Cancel" : "Close"}
                       </button>
                     </div>
                   </Dialog.Panel>
