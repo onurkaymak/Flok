3e9895aa3481cad5a148a544e5b52a5c14d47dd4
onurkaymak <onurkaymak@outlook.com>
2024-01-01 21:11:05 -0800

    Add event listener to Okay button.

--- src/components/roles/manager/rental/RentalServiceList.js
+++ src/components/roles/manager/rental/RentalServiceList.js
@@ -304,7 +304,7 @@ const RentalServiceList = (props) => {
                       <button
                         type="button"
                         className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
-                        onClick={() => setOpen(false)}
+                        onClick={modalCloseHandler}
                       >
                         Okay
                       </button>
