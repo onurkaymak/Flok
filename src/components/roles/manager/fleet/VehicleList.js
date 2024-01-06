be1ee5cd620e7d23676b911d3077d1fccb6d4fa5
onurkaymak <onurkaymak@outlook.com>
2024-01-05 20:43:52 -0800

    Fix VehicleList.

--- src/components/roles/manager/fleet/VehicleList.js
+++ src/components/roles/manager/fleet/VehicleList.js
@@ -33,19 +33,6 @@ const VehicleList = (props) => {
           checkboxSelection
         />
       </div>
-
-      {/* {props.fetchedVehicles.map(vehicle =>
-        <Vehicle
-          key={vehicle.vehicleId}
-          vehicleId={vehicle.vehicleId}
-          vin={vehicle.vin}
-          make={vehicle.make}
-          model={vehicle.model}
-          mileage={vehicle.mileage}
-          isRented={vehicle.isRented}
-          inProduction={vehicle.inProduction}
-        />
-      )} */}
     </React.Fragment>
   )
 };
