ddca4e1a864ecefcddd722f287d90dbae94c36d3
onurkaymak <onurkaymak@outlook.com>
2024-01-06 20:27:35 -0800

    Add UI Slice into the Redux store.

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user-slice';
 import fleetReducer from './fleet-slice';
 import rentalReducer from './rental-slice';
+import uiReducer from './ui-slice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     fleet: fleetReducer,
-    rental: rentalReducer
+    rental: rentalReducer,
+    ui: uiReducer
   }
 });
 
