59dce11e487011acd5a06272f33d22e47cd99a02
onurkaymak <onurkaymak@outlook.com>
2024-01-07 20:49:52 -0800

    WIP - Notification component.

--- src/components/UI/Notification.js
+++ src/components/UI/Notification.js
@@ -1,6 +1,88 @@
-const Notification = () => {
+import { Fragment, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
+
+import classes from './Notification.module.css';
+
+const Notification = ({ title, message }) => {
+  const [open, setOpen] = useState(true);
+
+  const modalCloseHandler = () => {
+
+  };
+
+
   return (
-    <h1>Notification</h1>
+    <div className={classes.selectedRentalServiceContainer}>
+      <Transition.Root show={open} as={Fragment} onClose={modalCloseHandler}>
+        <Dialog as="div" className="relative z-10">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+
+
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 flex justify-center	">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-14 w-14 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <InformationCircleIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                      </div>
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                          {title}
+                        </Dialog.Title>
+                        <div className="mt-4">
+                          <div className={classes.reservationInfoModalContainer}>
+                            <div className="border-t border-gray-600">
+                              <dl className="divide-y divide-gray-100">
+
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-1 sm:gap-4 sm:px-0">
+                                  <p>{message}</p>
+                                </div>
+
+                              </dl>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
+                    >
+                      Okay
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </div>
   )
 };
 
