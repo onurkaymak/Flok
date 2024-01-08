7ea899fd2e83248b643d41e04ac54ad0321b3fd0
onurkaymak <onurkaymak@outlook.com>
2024-01-07 21:28:57 -0800

    Create notification modal.

--- src/components/UI/Notification.js
+++ src/components/UI/Notification.js
@@ -42,19 +42,25 @@ const Notification = ({ title, message }) => {
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
 
 
-                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 flex justify-center	">
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                     <div className="sm:flex sm:items-start">
-                      <div className="mx-auto flex h-14 w-14 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
-                        <InformationCircleIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                      </div>
-                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                          {title}
-                        </Dialog.Title>
+                      {/* <div className="mx-auto flex h-14 w-14 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <InformationCircleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                      </div> */}
+                      <div className="mt-3 sm:ml-4 sm:mt-0 sm:text-center w-full">
+                        <div className="flex justify-center">
+                          <div className="mx-auto flex h-14 w-14 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <InformationCircleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                          </div>
+                          <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                            {title}
+                          </Dialog.Title>
+                        </div>
+
                         <div className="mt-4">
                           <div className={classes.reservationInfoModalContainer}>
                             <div className="border-t border-gray-600">
-                              <dl className="divide-y divide-gray-100">
+                              <dl className="divide-y divide-white-100">
 
                                 <div className="px-4 py-6 sm:grid sm:grid-cols-1 sm:gap-4 sm:px-0">
                                   <p>{message}</p>
@@ -82,7 +88,7 @@ const Notification = ({ title, message }) => {
           </div>
         </Dialog>
       </Transition.Root>
-    </div>
+    </div >
   )
 };
 
